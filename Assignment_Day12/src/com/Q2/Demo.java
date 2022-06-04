0a610a3991e4506160124ff8f446e2e0371c3f0e
brijeshkumarrai <brijeshrai9820@gmail.com>
2022-06-05 01:05:36 +0530

    day12 added

--- Assignment_Day12/src/com/Q2/Demo.java
+++ Assignment_Day12/src/com/Q2/Demo.java
@@ -1,13 +1,28 @@
 package com.Q2;
+import java.util.regex.Pattern;
 import java.util.Scanner;
 public class Demo {
 	Citizen citizen = new Citizen();
 	public boolean validate(String name, String mobileNum, String aadharCard) {
+		int flag = 0;
+		if(Pattern.matches("[a-zA-Z]{3,9}", name)) {
+			flag++;
+		}	
+		if(Pattern.matches("[6789]{1}[0-9]{9}", mobileNum)) {
+			flag++;
+		}
+		if(Pattern.matches("[0-9]{4}[0-9]{4}[0-9]{4}", aadharCard)) {
+			flag++;
+		}
+		if(flag==3) {
+			return true;
+		}
 		return false;
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Citizen citizen = new Citizen();
+		Demo d1 = new Demo();
 		
 		System.out.println("Enter your name :");
 		String name = sc.next();
@@ -17,11 +32,16 @@ public class Demo {
 		
 		System.out.println("Enter your aadharCard - ");
 		String aadharCard = sc.next();
+		boolean check = d1.validate(name, mobileNum, aadharCard);
+		if(check) {
+			citizen.setName(name);
+			citizen.setMobileNumber(mobileNum);
+			citizen.setAadharNumber(aadharCard);
+			citizen.showDetails();
+		}else {
+			System.out.println("Enter valid input!");
+		}
 		
-		citizen.setName(name);
-		citizen.setMobileNumber(mobileNum);
-		citizen.setAadharNumber(aadharCard);
-		citizen.showDetails();
 		sc.close();
 	}
 }
